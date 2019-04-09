efe1234bb70e7f28495ee544c6f6321dda8a7edd
Kevin Kirsche <kev.kirsche@gmail.com>
2019-04-09 10:06:41 -0400

    Add closing stuff

--- scp/upload.go
+++ scp/upload.go
@@ -3,7 +3,6 @@ package scp
 import (
 	"fmt"
 	"io"
-	"io/ioutil"
 	"os"
 
 	"github.com/pkg/errors"
@@ -17,22 +16,36 @@ func UploadFile(stdin io.WriteCloser, stdout io.Reader, path string) error {
 		return errors.Wrap(err, "failed to retrieve file information prior to file upload")
 	}
 
-	transferStartMessage := fmt.Sprintf("C%04o %d %s\n",
+	// C0644 12 test
+	// would mean: 12 byte file named test which has 0644 for it's filesystem permissions
+	beginMessage := fmt.Sprintf("C%04o %d %s\n",
 		fileInfo.Mode(),
 		fileInfo.Size(),
 		fileInfo.Name())
 
-	file, err := ioutil.ReadFile(path)
+	file, err := os.Open(path)
 	if err != nil {
 		return errors.Wrap(err, "failed to open file for transferring")
 	}
+	defer file.Close()
 
 	readBuffer := make([]byte, 1)
-	stdin.Write([]byte(transferStartMessage))
+	_, err = stdin.Write([]byte(beginMessage))
+	if err != nil {
+		return errors.Wrap(err, "failed to begin transfer")
+	}
 	stdout.Read(readBuffer)
-	stdin.Write(file)
+
+	_, err = io.Copy(stdin, file)
+	if err != nil {
+		return errors.Wrap(err, "failed to copy file contents")
+	}
 	stdout.Read(readBuffer)
-	stdin.Write([]byte("\x00"))
+
+	_, err = stdin.Write([]byte("\x00"))
+	if err != nil {
+		return errors.Wrap(err, "failed to terminate the connection")
+	}
 
 	return nil
 }
